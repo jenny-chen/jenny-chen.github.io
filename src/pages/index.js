08d7e4495d1ffdfce3a0bddf62a745a277531937
Jenny Chen <chen.jenny525@gmail.com>
2021-04-28 16:44:44 -0400

    Update index.js
    meant to put this in earlier but never got around it ;;

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,6 +74,9 @@ const IndexPage = () => {
         <Text>
           To be honest, I'm not sure what to do with this empty space yet. If you have a suggestion, I'd love to <ExtLink href="mailto:jenny.chen1@uwaterloo.ca">hear it</ExtLink>!
         </Text>
+        <Text>
+          I'm in the process of designing a new personal website, but for the moment, this layout is inspired by <ExtLink href="https://willium.com"/>William Strimling</ExtLink>.
+        </Text>
         <Text>In the meantime, I just updated this site with some memories from my <Link to="/japan">2019 trip to Japan</Link> and would love for you to see :D</Text>
       </Box>
     </Layout>
